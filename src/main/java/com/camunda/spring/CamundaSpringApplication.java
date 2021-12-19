d33e6259da5f248e02f34def51782d9a5799610b
Praveen Sana <spraveen0244@gmail.com>
2021-12-19 16:46:44 +0530

    fix(camunda web): added web dependency to see workflow in the UI

--- src/main/java/com/camunda/spring/CamundaSpringApplication.java
+++ src/main/java/com/camunda/spring/CamundaSpringApplication.java
@@ -1,12 +1,23 @@
 package com.camunda.spring;
 
+import org.camunda.bpm.engine.RuntimeService;
+import org.camunda.bpm.spring.boot.starter.event.PostDeployEvent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class CamundaSpringApplication {
 
+  @Autowired RuntimeService runtimeService;
+
   public static void main(String[] args) {
     SpringApplication.run(CamundaSpringApplication.class, args);
+  }
+
+  @EventListener
+  private void processPostDeploy(PostDeployEvent event) {
+    runtimeService.startProcessInstanceByKey("loan");
   }
 }
